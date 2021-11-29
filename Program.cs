ec4384523f233f9f02f6f135e2d7ee7d1ae917e5
NastasiaLu <nasta.lutchenko@gmail.com>
2021-11-29 04:08:30 +0200

    Triangle

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace QaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle1 = new Triangle();
+
+            Console.WriteLine("Input side x of dot A: ");
+            double Ax = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input side y of dot A: ");
+            double Ay = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input side x of dot B: ");
+            double Bx = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input side y of dot B: ");
+            double By = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input side x of dot C: ");
+            double Cx = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input side y of dot C: ");
+            double Cy = Convert.ToInt32(Console.ReadLine());
+
+            double AB = triangle1.LengtOfSide(Ax, Ay, Bx, By);
+            Console.WriteLine("Lenght of side AB: {0}", AB);
+
+            double BC = triangle1.LengtOfSide(Bx, By, Cx, Cy);
+            Console.WriteLine("Lenght of side BC: {0}", BC);
+
+            double AC = triangle1.LengtOfSide(Ax, Ay, Cx, Cy);
+            Console.WriteLine("Lenght of side AC: {0}", AC);
+
+
+            triangle1.GetType(AB, BC, AC);
+            triangle1.Triangle_Peremeter(AB, BC, AC);
+            triangle1.EvenNumbers();
+
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QaTest
+{
+    class Triangle
+    {
+        public double Perimeter;
+
+        public double LengtOfSide(double x1, double y1, double x2, double y2)
+        {
+            double SideLenght = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return SideLenght;
+
+        }
+
+
+        public void GetType(double a, double b, double c)
+        {
+            if (a == b && b == c && c == a)
+            {
+                Console.WriteLine("Triangle is equal");
+            }
+            else
+            {
+                Console.WriteLine("Triangle is not equal");
+            }
+
+            if (a == b || b == c || c == a)
+            {
+                Console.WriteLine("Triangle is Isosceles");
+            }
+            else
+            {
+                Console.WriteLine("Triangle is not Isosceles");
+
+            }
+            //if (Math.Pow(a, 2) + Math.Pow(c, 2) == Math.Pow(b, 2))
+            if (Math.Pow(b, 2) - (Math.Pow(a, 2) + Math.Pow(c, 2)) <= 1)
+            {
+                Console.WriteLine("Triangle is Right");
+            }
+            else
+            {
+                Console.WriteLine("Triangle is not Right");
+
+            }
+
+        }
+
+        public double Triangle_Peremeter(double a, double b, double c)
+        {
+            Perimeter = a + b + c;
+            Console.WriteLine("Peremetr is {0}", Perimeter);
+            return Perimeter;
+        }
+        public void EvenNumbers()
+        {
+            Console.WriteLine("Even numbers in range from 0 to triangle perimeter - {0}", Perimeter);
+
+            for (int i = 0; i <= Perimeter; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+
+        }
+    }
+}
